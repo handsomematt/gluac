375c62304e804260f9f4204145ad1522c1349ecb
Matt Stevens <matt@handsomematt.co.uk>
2017-03-07 03:18:12 +0000

    premake5 project file

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+solution "gluac"
+	configurations { "Debug", "Release" }
+	platforms { "x32", "x64" }
+
+	language		"C++"
+	characterset	"MBCS"
+	location		"project"
+	targetdir		"bin"
+
+	filter "platforms:x32"
+		architecture "x32"
+
+	filter "platforms:x64"
+		architecture "x64"
+
+	project "gluac"
+		kind	"ConsoleApp"
+		targetname "gluac"
+		flags { "Symbols", "NoEditAndContinue", "NoPCH", "StaticRuntime", "EnableSSE" }
+		links "bootil_static"
+		includedirs "Bootil/include"
+
+        if os.is( "linux" ) then
+                buildoptions { "-fPIC", "-pthread" }
+                linkoptions { "-pthread" }
+        end
+
+		files { "src/**.*" }
+
+	include "Bootil/projects/bootil_premake5.lua"
