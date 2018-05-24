5889b503dc4a11d0299f4bb7c7fa50448d0e0550
Matt Stevens <matt@handsomematt.co.uk>
2018-05-24 08:47:09 +0100

    read lua from stdin, output bytecode to stdout, open lua_shared_srv.so on linux so we don't need to sigscan, better error checking, remove all bootil shit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
-#include "Bootil/Bootil.h"
-
-using namespace Bootil;
-
 #include "lua_dyn.h"
 #include "lua_jit.h"
+
 #ifdef _WIN32
 #include <windows.h>
 #else
@@ -15,7 +12,9 @@ using namespace Bootil;
 #define LUA_PREFIX LuaFunctions.
 lua_All_functions LuaFunctions;
 
-String::List g_FileList;
+char* g_sFilename = nullptr;
+bool g_bParseOnly = false;
+bool g_bStripDebug = true;
 
 typedef int(__cdecl *lj_bcwrite_t) (lua_State *L, void *gcproto, lua_Writer, void *data, int strip);
 lj_bcwrite_t lj_bcwrite = NULL;
@@ -28,13 +27,12 @@ static const char *LuaJIT_bcwrite_sym = "@lj_bcwrite";
 static const size_t LuaJIT_bcwrite_symlen = 0;
 #endif
 
-static void PrintUsage()
+static void print_usage()
 {
-	Output::Msg("USAGE: gluac [filenames] [-?] [-o] [-p]\n");
-	Output::Msg("-?: Prints this message\n");
-	Output::Msg("-o: Output filename (default is output.luac)\n");
-	Output::Msg("-p: Parse only, doesn't dump bytecode\n");
-	Output::Msg("-s: Strip debug information\n");
+	printf("USAGE: gluac [filename] [-o] [-p] [-s]\n");
+	printf("-o: Output filename (default: stdout)\n");
+	printf("-p: Parse only, doesn't dump bytecode\n");
+	printf("-s: Strip debug information\n");
 
 	exit(1);
 }
@@ -64,110 +62,92 @@ int write_dump(lua_State *L, const void* p, size_t sz, void* ud)
 	return 0;
 }
 
-int lua_dump_with_strip(lua_State *L, lua_Writer writer, void *data, int strip)
+int lua_bcwrite(lua_State *L, lua_Writer writer, void *data, bool strip)
 {
 	cTValue *o = L->top - 1;
 	return lj_bcwrite(L, (GCproto *)(mref((&gcval(o)->fn)->l.pc, char) - sizeof(GCproto)), writer, data, strip);
 }
 
-static int lua_main(lua_State* L)
-{
-	BString output_filename = CommandLine::GetSwitch("-o", "output.ljc");
-
-	BOOTIL_FOREACH_CONST(file, g_FileList, String::List)
-	{
-		const char* filename = (*file).c_str();
-
-		if (luaL_loadfile(L, filename) != 0)
-		{
-			Output::Warning("%s\n", lua_tostring(L, -1));
-			continue;
-		}
-
-		if (CommandLine::HasSwitch("-p"))
-			continue;
-
-		FILE* D = fopen(output_filename.c_str(), "ab");
-		if (D == NULL)
-		{
-			Output::Warning("cannot open (%s)\n", strerror(errno));
-			continue;
-		}
-
-		char* bytecode = 0L;
-		size_t len = 0;
-		wdata wd = { &len, &bytecode };
-		
-		int strip = 0;
-		if (CommandLine::HasSwitch("-s"))
-			strip = 1;
-
-		if (lua_dump_with_strip(L, write_dump, &wd, strip))
-		{
-			Output::Warning("failed to dump bytecode\n");
-			continue;
-		}
-
-		fwrite(bytecode, len, 1, D);
-		
-		ferror(D);
-		fclose(D);
-	}
-
-	return 0;
-}
-
-bool LoadLuaShared()
+bool load_lua_shared()
 {
 	#ifdef _WIN32
-		HMODULE module = LoadLibrary("lua_shared.dll");
+	HMODULE module = LoadLibrary("lua_shared.dll");
+
+	if (module == nullptr) {
+		fprintf(stderr, "could not find lua_shared\n");
+		return false;
+	}
 	#else
-		void* module = dlopen("lua_shared.so", RTLD_LAZY);
+	void* module = dlopen("./lua_shared_srv.so", RTLD_LAZY);
+
+	if (module == nullptr) {
+		fprintf(stderr, "%s\n", dlerror());
+		return false;
+	}
+
 	#endif
 
 	SymbolFinder symfinder;
 	lj_bcwrite = reinterpret_cast<lj_bcwrite_t>(symfinder.Resolve(module, LuaJIT_bcwrite_sym, LuaJIT_bcwrite_symlen));
 	
+	if (lj_bcwrite == nullptr) {
+		fprintf(stderr, "failed to resolve lj_bcwrite\n");
+		return false;
+	}
+
 	return luaL_loadfunctions(module, &LuaFunctions, sizeof(LuaFunctions));
+}
+
+static int lua_main(lua_State* L)
+{
+	// load our Lua file as a chunk on the stack (if filename is NULL it loads from stdin)
+	if (luaL_loadfile(L, g_sFilename) != 0) {
+		fprintf(stderr, "luaL_loadfile: %s\n", lua_tostring(L, -1));
+		return 0;
+	}
+
+	// return early if we only want parsing
+	if (g_bParseOnly) {
+		return 0;
+	}
+
+	char* bytecode = 0L;
+	size_t len = 0;
+	wdata wd = { &len, &bytecode };
+
+	if (lua_bcwrite(L, write_dump, &wd, g_bStripDebug))
+	{
+		fprintf(stderr, "failed to dump bytecode\n");
+		return 0;
+	}
+
+	// output bytecode to stdout
+	fwrite(bytecode, len, 1, stdout);
+	fflush(stdout);
+	
+	return 0;
 }
 
 int main(int argc, char* argv[])
 {
-	Debug::SuppressPopups( true );
-	CommandLine::Set(argc, argv);
-
-	if (CommandLine::GetArgCount() <= 0 || CommandLine::HasSwitch("-?"))
-		PrintUsage();
-
-	for (int i = 0; i < CommandLine::GetArgCount(); i++)
-		if (String::Test::StartsWith(CommandLine::GetArg(i), "-"))
-			break;
-		else
-			g_FileList.push_back(CommandLine::GetArg(i));
-
-	if (g_FileList.empty())
-		PrintUsage();
-	
-	if (!LoadLuaShared())
-	{
-		Output::Warning("Error loading lua_shared.\n");
-		exit(1);
+	if (!load_lua_shared()) {
+		fprintf(stderr, "error loading lua_shared\n");
+		return 1;
 	}
 
-	lua_State* L;
-
-	L = lua_open();
+	lua_State* L = lua_open();
 	luaL_openlibs(L);
-	if (L == NULL)
-	{
-		Output::Warning("Cannot create state: not enough memory.\n");
-		exit(1);
+	if (L == nullptr) {
+		fprintf(stderr, "cannot create lua state: not enough memory.\n");
+		return 1;
 	}
 
-	if (lua_cpcall(L, lua_main, NULL) != 0)
-		Output::Warning("%s\n", lua_tostring(L, -1));
+	if (lua_cpcall(L, lua_main, nullptr) != 0) {
+		fprintf(stderr, "lua_cpcall: %s\n", lua_tostring(L, -1));
+		lua_close(L);
+		return 1;
+	}
 
 	lua_close(L);
 	return 0;
-
 }
